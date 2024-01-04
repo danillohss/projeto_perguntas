440b7866f69e38fc5476bd5ddc9ff112fe89c767
Danillo Soares <danillohss@gmail.com>
2024-01-04 19:26:51 -0300

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 main() => runApp(new PerguntaApp());
-
+//teste
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Olá flutter'),
