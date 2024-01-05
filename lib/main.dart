8df1e8111c2e38cedc7fc130a80377a18c5572c6
Danillo Soares <danillohss@gmail.com>
2024-01-05 18:43:00 -0300

    Flutter - Evoluindo app e estudo dos estados estático e dinamico

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(new PerguntaApp());
-//teste
-class PerguntaApp extends StatelessWidget {
+main() => runApp(PerguntaApp());
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print('Resposta aceita');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = ['Qual sua cor favorita?', 'Qual é seu animal favorito?'];
     return MaterialApp(
-      home: Text('Olá flutter'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('perguntas'),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas[perguntaSelecionada]),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 2')),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 3')),
+            ],
+          )),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
